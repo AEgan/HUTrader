300dc266410cdff5b37237d8903dd79c52a611d2
Alex Egan <alexander.egan@customink.com>
2015-07-22 16:20:31 -0400

    removes unused files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'contexts'
-require 'capybara/rails'
 class ActiveSupport::TestCase
   # hides sql output from unit tests
   ActiveRecord::Base.logger.level = 2
